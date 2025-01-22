90a08902ecd47ccff1139542656707d6d2e62457
Jeeems10 <jamesponce10@outlook.com>
2025-01-22 19:30:17 +0100

    - created Slider for brightness and Switch for ON/OFF

--- app/src/main/java/com/example/smartlightingapp/MainActivity.kt
+++ app/src/main/java/com/example/smartlightingapp/MainActivity.kt
@@ -3,13 +3,28 @@ package com.example.smartlightingapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.smartlightingapp.ui.theme.SmartLightingAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +37,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    HomeScreen()
                 }
             }
         }
@@ -30,17 +45,50 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun HomeScreen(
+    modifier: Modifier = Modifier
+) {
+    //State für Button und Slider
+    var isLightOn by remember { mutableStateOf(false) }
+    var brightness by remember { mutableStateOf(50) }
+
+    // Layout
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+
+    ) {
+        Text(text = "Smart Lighting")
+        
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(text = "Light is: ${if (isLightOn) "On" else "Off"}")
+            Spacer(modifier = Modifier.width(8.dp))
+            Switch(checked = isLightOn,
+                onCheckedChange = {isLightOn = it}
+            )
+        }
+        
+        Spacer(modifier = Modifier.height(20.dp))
+        
+        Text(text = "Brightsness:$brightness")
+        
+        Slider(value = brightness.toFloat(),
+            onValueChange = {
+                brightness = it.toInt()
+            },
+            valueRange = 0f..100f
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    SmartLightingAppTheme {
-        Greeting("Android")
-    }
 }
